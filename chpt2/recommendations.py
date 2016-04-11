a6eb9b159d67069d01ff73b096481367cb4e159c
andela-sjames <samuel.james@andela.com>
2016-04-11 11:31:52 +0100

    add pearson correlation to script.

--- chpt2/recommendations.py
+++ chpt2/recommendations.py
@@ -54,6 +54,7 @@ critics = {'Lisa Rose': {'Lady in the Water': 2.5,
 
 
 # Returns a distance-based similarity score for person1 and person2
+# euclidean distance
 def sim_distance(prefs, person1, person2):
     """Get the list of shared_items."""
     si = {}
@@ -67,3 +68,33 @@ def sim_distance(prefs, person1, person2):
     sum_of_squares = sum([pow(prefs[person1][item] - prefs[person2][item], 2)
                          for item in prefs[person1] if item in prefs[person2]])
     return 1 / (1 + sum_of_squares)
+
+
+# Returns the Pearson correlation coefficient for p1 and p2
+# pearson correlation score
+def sim_pearson(prefs, p1, p2):
+    """Get the list of mutually rated items."""
+    si = {}
+    for item in prefs[p1]:
+        if item in prefs[p2]:
+            si[item] = 1
+    # Find the number of elements
+    n = len(si)
+    # if they are no ratings in common, return 0
+    if n == 0:
+        return 0
+    # Add up all the preferences
+    sum1 = sum([prefs[p1][it] for it in si])
+    sum2 = sum([prefs[p2][it] for it in si])
+    # Sum up the squares
+    sum1sq = sum([pow(prefs[p1][it], 2) for it in si])
+    sum2sq = sum([pow(prefs[p2][it], 2) for it in si])
+    # Sum up the products
+    psum = sum([prefs[p1][it] * prefs[p2][it] for it in si])
+    # Calculate Pearson score
+    num = psum - (sum1 * sum2 / n)
+    den = sqrt((sum1sq - pow(sum1, 2) / n) * (sum2sq - pow(sum2, 2) / n))
+    if den == 0:
+        return 0
+    r = num / den
+    return r
