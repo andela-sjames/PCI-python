8b4b35b71ee52dfbef7148b31ad15da5b8d7d38a
andela-sjames <samuel.james@andela.com>
2016-04-16 22:33:56 +0100

    add recomendatios script.

--- chpt2/recommendations.py
+++ chpt2/recommendations.py
@@ -112,3 +112,35 @@ def top_matches(prefs, person, n=5, similarity=sim_pearson):
     scores.sort()
     scores.reverse()
     return scores[0:n]
+
+
+# Gets recommendations for a person by using a weighted average
+# of every other user's rankings
+def get_recommendations(prefs, person, similarity=sim_pearson):
+    totals = {}
+    simSums = {}
+    for other in prefs:
+        # don't compare me to myself
+        if other == person:
+            continue
+        sim = similarity(prefs, person, other)
+        # ignore scores of zero or lower
+        if sim <= 0:
+            continue
+        for item in prefs[other]:
+            # only score movies I haven't seen yet
+            if item not in prefs[person] or prefs[person][item] == 0:
+                # Similarity * Score
+                totals.setdefault(item, 0)
+                totals[item] += prefs[other][item] * sim
+
+                # Sum of similarities
+                simSums.setdefault(item, 0)
+                simSums[item] += sim
+
+            # Create the normalized list
+        rankings = [(total / simSums[item], item) for item, total in totals.items()]
+        # Return the sorted list
+        rankings.sort()
+        rankings.reverse()
+        return rankings
