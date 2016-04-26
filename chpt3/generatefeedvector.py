69d3a021db69a928b7e44c87293b174440004469
andela-sjames <samuel.james@andela.com>
2016-04-26 23:28:02 +0100

    add word count method

---
+++ chpt3/generatefeedvector.py
@@ -0,0 +1,20 @@
+import feedparser
+import re
+
+# Returns title and dictionary of word counts for an RSS feed
+def getwordcounts(url):
+  # Parse the feed
+  d=feedparser.parse(url)
+  wc={}
+
+  # Loop over all the entries
+  for e in d.entries:
+    if 'summary' in e: summary=e.summary
+    else: summary=e.description
+
+    # Extract a list of words
+    words=getwords(e.title+' '+summary)
+    for word in words:
+      wc.setdefault(word,0)
+      wc[word]+=1
+  return d.feed.title,wc
